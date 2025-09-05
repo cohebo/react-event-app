ca71494c22fc732064388da24dd8960b7814f54f
Cees Boot <ceesboot@gmail.com>
2025-09-05 17:41:36 +0200

    Implement events loader and update EventsPage to display fetched events (just titles)

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+import { useLoaderData } from "react-router-dom";
+
+export const eventsLoader = async () => {
+	const response = await fetch("http://localhost:3000/events");
+	if (!response.ok) {
+		throw new Error("Events could not be fetched");
+	}
+	return await response.json();
+};
 
 export const EventsPage = () => {
-  return <Heading>List of events</Heading>;
+	const events = useLoaderData();
+	return (
+		<>
+			<Heading>List of events</Heading>
+			{events && events.length > 0 ? events.map((event) => <div key={event.id}>{event.title}</div>) : <div>Geen events gevonden.</div>}
+		</>
+	);
 };
