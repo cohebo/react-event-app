df59df7b3bb0eebd61eb25272e33fc03d1aaf16b
Cees Boot <ceesboot@gmail.com>
2025-09-05 22:52:02 +0200

    Remove eventsLoader function as it is no longer needed

--- src/loaders/eventsLoader.js
+++
@@ -1,8 +0,0 @@
-// Loader functie voor het ophalen van events
-export async function eventsLoader() {
-	const response = await fetch("http://localhost:3000/events");
-	if (!response.ok) {
-		throw new Error("Events konden niet worden opgehaald");
-	}
-	return response.json();
-}
