355ed964e2d73c23d78aabb939cc74aaba1bac18
Cees Boot <ceesboot@gmail.com>
2025-09-05 18:19:14 +0200

    Add EventDateTime component for formatting and displaying event date, start and end times

---
+++ src/components/EventDateTime.jsx
@@ -0,0 +1,28 @@
+export default function EventDateTime({ start, end }) {
+	const startDate = new Date(start);
+	const endDate = new Date(end);
+	const dateString = startDate.toLocaleDateString("en-GB", {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+	});
+	const startTime = startDate.toLocaleTimeString("en-GB", {
+		hour: "2-digit",
+		minute: "2-digit",
+		hour12: false,
+	});
+	const endTime = endDate.toLocaleTimeString("en-GB", {
+		hour: "2-digit",
+		minute: "2-digit",
+		hour12: false,
+	});
+	return (
+		<>
+			<p>{dateString}</p>
+			<p>
+				{startTime} – {endTime}
+			</p>
+		</>
+	);
+}
