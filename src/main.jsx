c40253a560d890e7fdab06991335dbed8fc2dd24
Cees Boot <ceesboot@gmail.com>
2025-09-05 17:31:21 +0200

    Add loader function to Eventspage routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,33 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { EventPage } from './pages/EventPage';
-import { EventsPage } from './pages/EventsPage';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Root } from './components/Root';
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { EventPage } from "./pages/EventPage";
+import { EventsPage, eventsLoader } from "./pages/EventsPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root } from "./components/Root";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        path: '/',
-        element: <EventsPage />,
-        // loader: postListLoader,
-      },
-      {
-        path: '/event/:eventId',
-        element: <EventPage />,
-        // loader: postLoader,
-        // action: addComment,
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <Root />,
+		children: [
+			{
+				path: "/",
+				element: <EventsPage />,
+				loader: eventsLoader,
+			},
+			{
+				path: "events/:id",
+				element: <EventPage />,
+			},
+		],
+	},
 ]);
 // @ts-ignore
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<ChakraProvider>
+			<RouterProvider router={router} />
+		</ChakraProvider>
+	</React.StrictMode>
 );
