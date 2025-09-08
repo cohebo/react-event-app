4605ccd94a3c2c536f788232308173d816e22510
Cees Boot <ceesboot@gmail.com>
2025-09-08 11:15:11 +0200

    Style navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Flex, Link } from "@chakra-ui/react";
 
-export const Navigation = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Events</Link>
-        </li>
-        <li>
-          <Link to="/event/1">Event</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+export const Navigation = () => (
+	<Flex
+		as="nav"
+		bg="blue.600"
+		px={4}
+		h={16}
+		align="center"
+		gap={8}
+		boxShadow="sm">
+		<Link
+			as={RouterLink}
+			to="/"
+			color="white"
+			fontWeight="bold"
+			_hover={{ textDecoration: "none", color: "blue.300" }}>
+			EventApp
+		</Link>
+	</Flex>
+);
