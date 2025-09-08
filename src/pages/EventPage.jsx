8762a92797590338a06354f2765ed787dc7c3c20
Cees Boot <ceesboot@gmail.com>
2025-09-08 11:14:31 +0200

    Simplify EventPage layout

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Container, Stack, Image, Heading, Text, Badge, AspectRatio } from "@chakra-ui/react";
+import { Box, Container, Stack, Image, Heading, Text, Badge } from "@chakra-ui/react";
 import { useLoaderData } from "react-router-dom";
 import formatEventDateTime from "../helpers/formatEventDateTime";
 
@@ -15,6 +15,8 @@ export const eventLoader = async ({ params }) => {
 export const EventPage = () => {
 	const event = useLoaderData();
 	const eventAuthor = event.users?.find((u) => u.id === event.createdBy);
+	const { dateString, timeString } = formatEventDateTime(event.startTime, event.endTime);
+	const categories = event.categoryIds.map((id) => event.categories.find((c) => c.id === id)).filter(Boolean);
 	return (
 		<Box bg="gray.50">
 			<Container
@@ -26,43 +28,34 @@ export const EventPage = () => {
 					borderRadius="md"
 					overflow="hidden"
 					boxShadow="md"
-					h="100%"
 					bg="white">
 					<Stack
-						direction={["column", "row"]}
+						direction="column"
 						w="100%"
-						h={["auto", "500px"]}
 						spacing={0}>
 						<Box
-							w={["100%", "50%"]}
+							w="100%"
 							minW={0}>
-							<AspectRatio
-								ratio={1}
+							<Image
+								src={event.image}
+								alt={event.title}
+								objectFit="cover"
+								borderTopRadius={["md", 0]}
+								borderLeftRadius={[0, "md"]}
+								borderBottomRadius={0}
+								borderRightRadius={0}
+								m={0}
+								display="block"
 								w="100%"
-								h={["200px", "100%"]}>
-								<Image
-									src={event.image}
-									alt={event.title}
-									objectFit="cover"
-									borderTopRadius={["md", 0]}
-									borderLeftRadius={[0, "md"]}
-									borderBottomRadius={0}
-									borderRightRadius={0}
-									m={0}
-									display="block"
-								/>
-							</AspectRatio>
+								maxHeight={{ base: "300px", md: "400px", lg: "500px" }}
+							/>
 						</Box>
 						<Box
-							w={["100%", "50%"]}
-							h={["auto", "100%"]}
+							w="100%"
 							minW={0}
 							pt={6}
 							pb={4}
-							px={6}
-							display="flex"
-							alignItems="flex-start"
-							justifyContent="center">
+							px={6}>
 							<Stack
 								spacing={3}
 								w="100%">
@@ -79,42 +72,32 @@ export const EventPage = () => {
 								<Text
 									color="gray.500"
 									fontSize="sm">
-									Location: {event.location}
+									Locatie: {event.location}
 								</Text>
-								{(() => {
-									const { dateString, timeString } = formatEventDateTime(event.startTime, event.endTime);
-									return (
-										<>
-											<Text
-												fontSize="sm"
-												color="gray.600">
-												{dateString}
-											</Text>
-											<Text
-												fontSize="sm"
-												color="gray.600">
-												{timeString}
-											</Text>
-										</>
-									);
-								})()}
+								<Text
+									fontSize="sm"
+									color="gray.600">
+									{dateString}
+								</Text>
+								<Text
+									fontSize="sm"
+									color="gray.600">
+									{timeString}
+								</Text>
 								<Stack
 									direction="row"
 									spacing={2}>
-									{event.categoryIds.map(
-										(id) =>
-											event.categories.find((category) => category.id === id) && (
-												<Badge
-													key={id}
-													color="blue.500"
-													size="sm"
-													borderRadius="full"
-													px={2}
-													py={1}>
-													{event.categories.find((category) => category.id === id).name}
-												</Badge>
-											)
-									)}
+									{categories.map((cat) => (
+										<Badge
+											key={cat.id}
+											color="blue.500"
+											size="sm"
+											borderRadius="full"
+											px={2}
+											py={1}>
+											{cat.name}
+										</Badge>
+									))}
 								</Stack>
 								{eventAuthor ? (
 									<Box
