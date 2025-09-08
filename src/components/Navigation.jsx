2699626e509dcd82bf16098a8d582b740b67dafe
Cees Boot <ceesboot@gmail.com>
2025-09-08 11:51:14 +0200

    Add button to Navigation component for adding event

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,23 +1,45 @@
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { Flex, Link } from "@chakra-ui/react";
+import { Flex, Box, Link } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 
 export const Navigation = () => (
 	<Flex
 		as="nav"
 		bg="blue.600"
-		px={4}
-		h={16}
-		align="center"
-		gap={8}
-		boxShadow="sm">
-		<Link
-			as={RouterLink}
-			to="/"
-			color="white"
-			fontWeight="bold"
-			_hover={{ textDecoration: "none", color: "blue.300" }}>
-			EventApp
-		</Link>
+		boxShadow="sm"
+		py={2}>
+		<Box
+			maxW="1200px"
+			w="100%"
+			mx="auto"
+			px={4}
+			display="flex"
+			alignItems="center"
+			justifyContent="space-between">
+			<Link
+				as={RouterLink}
+				to="/"
+				color="white"
+				fontWeight="bold"
+				fontSize="lg"
+				_hover={{ textDecoration: "none", color: "blue.300" }}>
+				EventApp
+			</Link>
+			<Link
+				as={RouterLink}
+				to="/add-event"
+				bg="white"
+				color="blue.600"
+				fontWeight="bold"
+				px={4}
+				py={2}
+				borderRadius="md"
+				ml={4}
+				_hover={{ bg: "blue.100", textDecoration: "none" }}>
+				<AddIcon mr={2} />
+				Add Event
+			</Link>
+		</Box>
 	</Flex>
 );
