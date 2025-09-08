4e6b9301c538682d8d53d9aca6c528b2cafbbd96
Cees Boot <ceesboot@gmail.com>
2025-09-08 12:17:41 +0200

    Refactor Navigation component to include AddEventModal and  event handling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,45 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { Flex, Box, Link } from "@chakra-ui/react";
+import { Flex, Box, Link, Button } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import { AddEventModal } from "./AddEventModal";
 
-export const Navigation = () => (
-	<Flex
-		as="nav"
-		bg="blue.600"
-		boxShadow="sm"
-		py={2}>
-		<Box
-			maxW="1200px"
-			w="100%"
-			mx="auto"
-			px={4}
-			display="flex"
-			alignItems="center"
-			justifyContent="space-between">
-			<Link
-				as={RouterLink}
-				to="/"
-				color="white"
-				fontWeight="bold"
-				fontSize="lg"
-				_hover={{ textDecoration: "none", color: "blue.300" }}>
-				EventApp
-			</Link>
-			<Link
-				as={RouterLink}
-				to="/add-event"
-				bg="white"
-				color="blue.600"
-				fontWeight="bold"
+export const Navigation = () => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const openModal = () => setIsModalOpen(true);
+	const closeModal = () => setIsModalOpen(false);
+
+	return (
+		<Flex
+			as="nav"
+			bg="blue.600"
+			boxShadow="sm"
+			py={4}>
+			<Box
+				maxW="1200px"
+				w="100%"
+				mx="auto"
 				px={4}
-				py={2}
-				borderRadius="md"
-				ml={4}
-				_hover={{ bg: "blue.100", textDecoration: "none" }}>
-				<AddIcon mr={2} />
-				Add Event
-			</Link>
-		</Box>
-	</Flex>
-);
+				display="flex"
+				alignItems="center"
+				justifyContent="space-between">
+				<Link
+					as={RouterLink}
+					to="/"
+					color="white"
+					fontWeight="bold"
+					fontSize="2xl"
+					_hover={{ textDecoration: "none", color: "blue.300" }}>
+					EventApp
+				</Link>
+				<Button
+					bg="white"
+					color="blue.600"
+					fontWeight="bold"
+					px={4}
+					py={2}
+					borderRadius="md"
+					ml={4}
+					_hover={{ bg: "blue.100" }}
+					leftIcon={<AddIcon />}
+					onClick={openModal}>
+					Add Event
+				</Button>
+				<AddEventModal
+					isOpen={isModalOpen}
+					onClose={closeModal}
+				/>
+			</Box>
+		</Flex>
+	);
+};
