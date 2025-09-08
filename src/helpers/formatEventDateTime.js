2d879b0551df1a46f167425f84c7bbdde8e7be18
Cees Boot <ceesboot@gmail.com>
2025-09-08 16:34:26 +0200

    Delete comment

--- src/helpers/formatEventDateTime.js
+++ src/helpers/formatEventDateTime.js
@@ -1,22 +1,21 @@
-// Geeft een object met een geformatteerde datum- en tijdstring voor een event
 export default function formatEventDateTime(start, end) {
-  const startDate = new Date(start);
-  const endDate = new Date(end);
-  const dateString = startDate.toLocaleDateString("en-GB", {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  });
-  const startTime = startDate.toLocaleTimeString("en-GB", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-  });
-  const endTime = endDate.toLocaleTimeString("en-GB", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-  });
-  return { dateString, timeString: `${startTime} – ${endTime}` };
+	const startDate = new Date(start);
+	const endDate = new Date(end);
+	const dateString = startDate.toLocaleDateString("en-GB", {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+	});
+	const startTime = startDate.toLocaleTimeString("en-GB", {
+		hour: "2-digit",
+		minute: "2-digit",
+		hour12: false,
+	});
+	const endTime = endDate.toLocaleTimeString("en-GB", {
+		hour: "2-digit",
+		minute: "2-digit",
+		hour12: false,
+	});
+	return { dateString, timeString: `${startTime} – ${endTime}` };
 }
