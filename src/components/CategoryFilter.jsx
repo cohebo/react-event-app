2d5723fe8c5f92f86fa576a5f304335960d17403
Cees Boot <ceesboot@gmail.com>
2025-09-08 16:38:37 +0200

    Use AppContext in CategoryFilter for category data

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,22 +1,25 @@
-import React from "react";
 import { Select } from "@chakra-ui/react";
+import { useAppContext } from "./AppContext";
 
-export const CategoryFilter = ({ categories, selectedCategory, onChange, ...props }) => (
-	<Select
-		placeholder="Select category"
-		value={selectedCategory}
-		onChange={onChange}
-		width="100%"
-		maxWidth={["100%", "50%", "380px"]}
-		bg="white"
-		borderRadius={8}
-		{...props}>
-		{categories.map((category) => (
-			<option
-				key={category.id}
-				value={category.id}>
-				{category.name}
-			</option>
-		))}
-	</Select>
-);
+export const CategoryFilter = ({ selectedCategory, onChange, ...props }) => {
+	const { categories } = useAppContext();
+	return (
+		<Select
+			placeholder="Select category"
+			value={selectedCategory}
+			onChange={onChange}
+			width="100%"
+			maxWidth={["100%", "50%", "380px"]}
+			bg="white"
+			borderRadius={8}
+			{...props}>
+			{categories.map((category) => (
+				<option
+					key={category.id}
+					value={category.id}>
+					{category.name}
+				</option>
+			))}
+		</Select>
+	);
+};
