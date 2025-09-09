f9dd42d47dd3aa523c1fa9657196ca1b78822bf0
Cees Boot <ceesboot@gmail.com>
2025-09-09 11:49:47 +0200

    Add label to CategoryFilter

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,23 +1,32 @@
-import { CheckboxGroup, Checkbox, Stack } from "@chakra-ui/react";
+import { CheckboxGroup, Checkbox, Stack, FormLabel, Box } from "@chakra-ui/react";
 import { useAppContext } from "./AppContext";
 
 export const CategoryFilter = ({ selectedCategory, onChange, ...props }) => {
 	const { categories } = useAppContext();
 	return (
-		<CheckboxGroup
-			colorScheme="blue"
-			value={selectedCategory}
-			onChange={onChange}
-			{...props}>
-			<Stack direction="row">
-				{categories.map((category) => (
-					<Checkbox
-						key={category.id}
-						value={String(category.id)}>
-						{category.name}
-					</Checkbox>
-				))}
-			</Stack>
-		</CheckboxGroup>
+		<Box>
+			<FormLabel
+				mb={0.5}
+				fontSize="sm"
+				fontWeight="semibold"
+				color="gray.600">
+				Filter by categories
+			</FormLabel>
+			<CheckboxGroup
+				colorScheme="blue"
+				value={selectedCategory}
+				onChange={onChange}
+				{...props}>
+				<Stack direction="row">
+					{categories.map((category) => (
+						<Checkbox
+							key={category.id}
+							value={String(category.id)}>
+							{category.name}
+						</Checkbox>
+					))}
+				</Stack>
+			</CheckboxGroup>
+		</Box>
 	);
 };
